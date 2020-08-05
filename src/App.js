a8c029d1e811fccf047c420a937f14bf8baa1468
tta2yta@gmail.com <tta2yta@gmail.com>
2020-08-05 14:36:40 +0300

    edit index.html

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ import Selectedrow from './components/Selectedrow'
    }
 
 componentDidMount(){
-  const url="http://www.filltext.com/?rows=32&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D";
+  const url="https://www.filltext.com/?rows=32&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D";
   fetch(url, {
     method:"GET"}).then(response=> response.json()).then(posts=>{
      this.setState({posts: posts})
